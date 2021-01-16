e842de638a6c81600fa2fb4d8a93e8d3686239ce
tabayoshi <tabayoshi53@gmail.com>
2021-01-16 18:38:41 +0900

    index.php edit.php
    index.phpにcssを追加
    投稿に対するコメントを追加するためのedit.phpを追加

--- edit.php
+++ edit.php
@@ -1,13 +1,38 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="device-width, initial-scale=1.0">
-    <title>Laravel News</title>
-  </head>
-  <body>
-    <div>
-    <h1>Laravel News</h1>
-    </div>
-  </body>
-</html>  
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Laravel News</title>
+  <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+  <header>
+    <p>Laravel News</p>
+  </header>
+  <div>
+    <?php foreach ($rows as $row): ?>
+      <h3><?=$row[0]?></h3><br>
+      <p><?=$row[1]?></p>
+      <hr>
+    <?php endforeach; ?>
+  </div>
+  <div class="comment_box">
+    <form action="" id="" method="POST">
+      <div>
+        <textarea class="comment" type="text" name="comment" value=""></textarea>
+      </div>
+      <div>
+        <input type="submit" class="comment_btn" name="button" value="コメントを書く">
+      </div>
+    </form>
+  </div>
+  <div>
+    <a href="index.php">元のページに戻る</a>
+  </div>
+</body>
+</html>
